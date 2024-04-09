52d7fbf4e1ecf05cd67631777e8726399007be3b
polte <polte@LAPTOP-0USHJ8FL>
2024-04-09 19:20:30 +0200

    Mensaje hola

---
+++ src/PruebaGit.java
@@ -0,0 +1,15 @@
+
+public class PruebaGit {
+	public static void main(String[] args) {
+		System.out.println("Hola");
+		
+	}
+	
+	public static int suma(int a,int b){
+		return a+b;
+	}
+	
+	public static int resta(int a, int b) {
+		return a+b;
+	}
+}
